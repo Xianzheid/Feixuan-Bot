12dba695538313188818c5c912fb70f89fe90107
Xianzhe ID <115347633+Xianzheid@users.noreply.github.com>
2022-10-18 01:11:19 +0700

    Update lolicon.js

--- plugins/lolicon.js
+++ plugins/lolicon.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/Xianzheid/Bahan-Nsfw/ma
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak', [['Next', '/${command}']], m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak',['Next',[.${command}]], m)
 }
 handler.command = /^(loli|lolicon)$/i
 handler.tags = ['anime']

59d8b1e027fde1a64b1be525d9e209cd797bda07
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-18 14:12:18 +0700

    Update lolicon.js

--- plugins/lolicon.js
+++ plugins/lolicon.js
@@ -1,5 +1,4 @@
-import fetch from 'node-fetch'
-
+/*import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 let res = await fetch('https://raw.githubusercontent.com/Xianzheid/Bahan-Nsfw/main/loli.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
@@ -10,4 +9,11 @@ await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak', '
 handler.command = /^(loli|lolicon)$/i
 handler.tags = ['anime']
 handler.premium = true
-export default handler
+export default handler*/
+
+import axios from "axios"
+
+let handler = async (m, {command, conn}) => {
+if (command == 'Lolicon') {
+let haha = await conn.getFile(`https://raw.githubusercontent.com/Xianzhe-ID/XianzheV1-MD/XianzheID/loli.json`)
+conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['🔄 NEXT 🔄', `/${command}`]], m)}

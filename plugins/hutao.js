5509b3b9895bdbaf9f5586a088c4b4f25f869262
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-19 14:51:39 +0700

    Add files via upload

---
+++ plugins/hutao.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheV1-MD/XianzheID/hutao.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , '©XianzheID', '.Hutao', m)
+}
+handler.command = /^(hutao)$/i
+handler.tags = ['premium']
+handler.help = ['hutao', 'premium']
+handler.premium = true
+export default handler

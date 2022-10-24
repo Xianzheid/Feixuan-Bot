139643eb385f454c57e0aafac8a66d935025f6b4
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-24 13:09:07 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,10 +1,30 @@
 import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let handler = async (m, { conn, command }) => {
-	let url = 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg'
-	conn.sendButton(m.chat, 'Nih..\nhttps://github.com/Xianzhe-ID/XiaFeixuan-Bot\n\n\nJangan Lupa Donasi:v', wm, await(await fetch(url)).buffer(), [['Donasi',`.donasi`]],m)
+let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
 
+Pengen mentahan nya ?
+Nih ada santai bro....
+https://github.com/ImYanXiao/Elaina-MultiDevice
 
+Pengen yang udah di recode oleh owner?
+Script bot ini dijual bila kalian mau bisa chat owner
+
+wa.me/6282127487538`
+let wibu = `https://telegra.ph/file/ddda3405b847c58198684.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/xianzhe_xx",
+    mediaType: "image",
+    description: "Instagram", 
+    title: 'XiaFeixuan Bot | MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig
+  }
+  } }) 
+          }
 handler.help = ['source code']
 handler.tags = ['info']
 handler.command =  /^(script|sc)$/i

ade96c844750b19c74bf00000c3f11416d16bb66
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-24 13:00:54 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,9 +1,8 @@
 import fetch from 'node-fetch'
 
-let text = `Nih..\nhttps://github.com/Xianzhe-ID/XiaFeixuan-Bot\n\n\nJangan Lupa Donasi:v`
 let handler = async (m, { conn, command }) => {
 	let url = 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg'
-	conn.sendButton(m.chat, text, wm, await(await fetch(url)).buffer(), [['Donasi',`.donasi`]],m)
+	conn.sendButton(m.chat, 'Nih..\nhttps://github.com/Xianzhe-ID/XiaFeixuan-Bot\n\n\nJangan Lupa Donasi:v', wm, await(await fetch(url)).buffer(), [['Donasi',`.donasi`]],m)
 
 
 handler.help = ['source code']

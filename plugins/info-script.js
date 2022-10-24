846ce5d26811a9868c50a4cb99584ac075724f9f
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-24 12:36:10 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,18 +5,9 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let str = `*https://github.com/Xianzhe-ID/XiaFeixuan-Bot*
 
 `
-/*let wibu = `https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg` 
-let thumb = await(await fetch(wibu)).buffer()*/
-conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg",
-    mediaType: "image",
-    description: "https://instagram.com/xianzhe_xx", 
-    title: 'XiaFeixuan-Bot | MultiDevice',
-    body: wm,
-    thumbnail: thumb,
-    sourceUrl: sig
-  }
-  } }) 
+let wibu = `https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
           }
 handler.help = ['source code']
 handler.tags = ['info']

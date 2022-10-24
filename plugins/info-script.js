52d2bae49a02370177a55291be2956f5b1a8164b
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-24 12:55:59 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,14 +1,11 @@
 import fetch from 'node-fetch'
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/Xianzhe-ID/XiaFeixuan-Bot*
+let text = `Nih..\nhttps://github.com/Xianzhe-ID/XiaFeixuan-Bot\n\n\nJangan Lupa Donasi:v`
+let handler = async (m, { conn, command }) => {
+	let url = 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg'
+	conn.sendButton(m.chat, text, wm, await(await fetch(url)).buffer(), [['Donasi',`.donasi`]],m)
 
-`
-let wibu = `https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg` 
-let thumb = await(await fetch(wibu)).buffer()
-conn.sendButton( m.chat, str, wm, null, [`Menu`, `.menu`], m)
-          }
+
 handler.help = ['source code']
 handler.tags = ['info']
 handler.command =  /^(script|sc)$/i

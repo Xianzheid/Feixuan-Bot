cb521a91aaed475f6bf5ff358dc4ad81aaff2afb
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-24 12:24:38 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,18 +5,18 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let str = `*https://github.com/Xianzhe-ID/XiaFeixuan-Bot*
 
 `
-let wibu = `https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg` 
-let thumb = await(await fetch(wibu)).buffer()
-/*conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/xianzhe_xx",
-    mediaType: "image/jpg",
+/*let wibu = `https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg` 
+let thumb = await(await fetch(wibu)).buffer()*/
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg",
+    mediaType: "image",
     description: "https://instagram.com/xianzhe_xx", 
-    title: 'XiaFeixyan-Bot | MultiDevice',
+    title: 'XiaFeixuan-Bot | MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
   }
-  } })*/
+  } }) 
           }
 handler.help = ['source code']
 handler.tags = ['info']

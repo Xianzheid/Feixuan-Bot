add5a5e5e8a37f6c1724f3f05605961838616c35
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-26 00:37:59 +0700

    Update test button.js

--- plugins/test button.js
+++ plugins/test button.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NS
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , [['Next', 'usedPrefix+${command}'], ['Menu', 'usedPrefix+menu']],  m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , [['Next', '/testbutton'], ['Menu', '/menu']],  m)
 }
 handler.command = /^(testbutton)$/i
 handler.tags = ['premium']

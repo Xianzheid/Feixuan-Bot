f80b0aa96409eeadde9003e9a5883190e0be4ed7
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-26 00:53:50 +0700

    Update test button.js

--- plugins/test button.js
+++ plugins/test button.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NSFW/XianzheID/CHAR/hutao.json')
+let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NSFW/JSON-FILE/loli.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , [['Next', '/testbutton'], ['Menu', '/menu']],  m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , [['Next', '/loli'], ['Menu', '/menu']],  m)
 }
-handler.command = /^(testbutton)$/i
+handler.command = /^(loli)$/i
 handler.tags = ['premium']
 handler.premium = true
 export default handler

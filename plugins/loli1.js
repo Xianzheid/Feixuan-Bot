ccf8b9a285381cb3fbbc390869e8bd0908f08530
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-26 01:55:58 +0700

    Update and rename test button.js to loli1.js

--- plugins/loli1.js
+++ plugins/loli1.js
@@ -5,9 +5,10 @@ let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NS
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , ['Next', '/loli'],  m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , ['Next', '.loli1'], m)
 }
-handler.command = /^(loli)$/i
+handler.command = /^(loli1)$/i
 handler.tags = ['premium']
+handler.help = ['loli', 'premium']
 handler.premium = true
 export default handler

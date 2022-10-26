40528deeb21f2465c21be74e4b804bf16629f98c
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-26 16:41:09 +0700

    Update hutao.js

--- plugins/hutao.js
+++ plugins/hutao.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, usedPrefix, command}) => {
 let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NSFW/XianzheID/JSON-FILE/hutao.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
-let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , '©XianzheID', ['Next', `.Hutao`], m)
+let url = json[Math.floor(Math.random)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , '©XianzheID', ['Next', `.${command}`], m)
 }
 handler.command = /^(hutao)$/i
 handler.tags = ['premium']

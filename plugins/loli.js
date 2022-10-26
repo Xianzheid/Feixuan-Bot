642b81d63e1aa74b7bf162f107cf777bf4b1edfe
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-26 16:49:16 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NSFW/XianzheID/JSON-FILE/loli.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
-let url = json[Math.floor(Math.random() * json.length)]
+let url = json[Math.floor(Math.random())]
 await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , '©XianzheID', '.Loli', m)
 }
 handler.command = /^(loli)$/i

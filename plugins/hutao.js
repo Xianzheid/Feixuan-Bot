709b2b7065e8d98a2c0ca82b827811321a07799c
Xianzhe ID <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-26 16:29:52 +0700

    Update hutao.js

--- plugins/hutao.js
+++ plugins/hutao.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/Xianzhe-ID/XianzheID-NS
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , '©XianzheID', ['Next','.Hutao'], m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Nih kak' , '©XianzheID', ['Next', `.Hutao`], m)
 }
 handler.command = /^(hutao)$/i
 handler.tags = ['premium']

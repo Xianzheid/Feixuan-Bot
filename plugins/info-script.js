fd347caa568eb6bb245208e3d77badef29aa33c1
XFS-Team Xianzhe <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-27 22:22:56 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,16 +2,16 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/XianzheID/XiaFeixuat-Bot*
-
+let str = `Nih kasih star ye
+https://github.com/xianzhe-id
 `
 let wibu = `https://telegra.ph/file/7f49101f4824088555a38.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/xianzhe_xx",
+    mediaUrl: "https://telegra.ph/file/3d62ea9b4201b4c3502fe.jpg",
     mediaType: "image/jpg",
     description: "https://instagram.com/xianzhe_xx", 
-    title: 'XiaFeixyan-Bot | MultiDevice',
+    title: 'XiaFeixuan-Bot | MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig

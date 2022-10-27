84d1981301589acb4f8ed2b4e4f232ec0ee5adfa
XFS-Team Xianzhe <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-27 17:00:00 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, text }) => {
     conn.sendButton(m.chat, 'Nih kak', 'https://instagram.com/xianzhe_xx', url, [['NEXT', '.loli']], m)
   }
   handler.command = /^(loli)$/i
-  handler.tags = ['premium']
-  handler.help = ['loli']
-  handler.premium = true
+  handler.tags = ['limit']
+  handler.help = ['limitmenu']
+  handler.register = true
   export default handler
   
  global.sange = ["https://iili.io/DRPGJn.jpg",

95c2e79172d9d10e8cefe625f3f866db38812f42
XFS-Team Xianzhe <116034496+Xianzhe-ID@users.noreply.github.com>
2022-10-28 02:51:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -157,7 +157,7 @@ global.thumbdoc = 'https://telegra.ph/file/5b0e97331a147f2036888.jpg'
 /*============== LOGO ==============*/
 global.thumb = 'https://telegra.ph/file/5b0e97331a147f2036888.jpg' //Main Thumbnail
 global.imagebot = 'https://telegra.ph/file/5b0e97331a147f2036888.jpg'
-global.giflogo = 'https://telegra.ph/file/2cd3626c740ba091c98ff.jpg'
+global.giflogo = 'https://i.imgur.com/looze9c.mp4.'
 global.thumbs = 'https://telegra.ph/file/5b0e97331a147f2036888.jpg'
 global.fla = 'https://telegra.ph/file/2cd3626c740ba091c98ff.jpg'
 global.thumbnailUrl = [
@@ -175,7 +175,7 @@ global.flaaa = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
-global.hwaifu2 = ['https://telegra.ph/file/2cd3626c740ba091c98ff.jpg', 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg', 'https://telegra.ph/file/590f94b0907c9cafb7494.jpg', 'https://telegra.ph/file/2cd3626c740ba091c98ff.jpg', 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg', 'https://telegra.ph/file/590f94b0907c9cafb7494.jpg', 'https://telegra.ph/file/ddda3405b847c58198684.jpg']
+global.hwaifu2 = '___'//['https://telegra.ph/file/2cd3626c740ba091c98ff.jpg', 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg', 'https://telegra.ph/file/590f94b0907c9cafb7494.jpg', 'https://telegra.ph/file/2cd3626c740ba091c98ff.jpg', 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg', 'https://telegra.ph/file/590f94b0907c9cafb7494.jpg', 'https://telegra.ph/file/ddda3405b847c58198684.jpg']
 
 /*============== HENTAI ==============*/ 
 //OPTIONS
@@ -184,8 +184,8 @@ global.premnsfw = true // Nsfw Mode Premium?
 
 
 //Ini cuma simpenan owner kok :v
-global.hoppai = ['https://telegra.ph/file/f34d76df4a2065af1a5ba.jpg','https://telegra.ph/file/05c1b22ee83bcd7723b4d.jpg','https://telegra.ph/file/1d886f66a44871205335f.jpg','https://telegra.ph/file/54d19a9094dc509370bf9.jpg','https://telegra.ph/file/e649475bcde78a9977ee5.jpg','https://telegra.ph/file/32ba20b6139b129c559c8.jpg','https://telegra.ph/file/948434cda49e4af5d9f11.jpg','https://telegra.ph/file/6f353c68533283fe79871.jpg','https://telegra.ph/file/de268ca9b01101acf2b8a.jpg','https://telegra.ph/file/fc6c5b2ae9a20c4256e7f.jpg', 'https://telegra.ph/file/efb70bb0988640f841742.jpg', 'https://telegra.ph/file/77d03fff530a2bcff3bf7.jpg','https://telegra.ph/file/6e4623464a659dd8d902b.jpg','https://telegra.ph/file/685aa39f0cb0f2c4fd85b.jpg','https://telegra.ph/file/10454b9ad693e1eefea58.jpg','https://telegra.ph/file/7de8ce5c290c3d8be0856.jpg','https://telegra.ph/file/8d7c4eadb7a4722747074.jpg','https://telegra.ph/file/ccc5f8eaac0f30919ef6c.jpg','https://telegra.ph/file/95f4b43439d7888f15ea5.jpg','https://telegra.ph/file/9c2a750db555bd2fad1f3.jpg', 'https://telegra.ph/file/efc5f7e637744fd6bfec2.jpg','https://telegra.ph/file/3a5231aade245665633bd.jpg','https://telegra.ph/file/2ecfc76feb26ec1eab99b.jpg','https://telegra.ph/file/dabb70983b4e833d98344.jpg','https://telegra.ph/file/75193a893e38fc580afe6.jpg','https://telegra.ph/file/217aa0f4ec76273808aa4.jpg','https://telegra.ph/file/8a35d3446b97ae22c7b23.jpg','https://telegra.ph/file/092df720701575a7ceaaa.jpg','https://telegra.ph/file/a65184a676cd648de34c3.jpg',
-'https://telegra.ph/file/180e28807e78419d45537.jpg','https://telegra.ph/file/140eff27be983e0cd6781.jpg','https://telegra.ph/file/1581b791e16d0029e16b5.jpg','https://telegra.ph/file/6a4b36372b4f265bae3bc.jpg','https://telegra.ph/file/093caff422f194f00bc6c.jpg','https://telegra.ph/file/2294b7ab49eca8a8046b2.jpg','https://telegra.ph/file/869224d1c417e8b5c8ff1.jpg','https://telegra.ph/file/a78443f0ee887f46808d7.jpg','https://telegra.ph/file/1889878933264d16c58bf.jpg','https://telegra.ph/file/735aeb47d9c4aa87aaaf3.jpg','https://telegra.ph/file/fcf861516db09dda164e0.jpg','https://telegra.ph/file/355d96d7e06d109435f67.jpg']
+global.hoppai = '____'
+//['https://telegra.ph/file/f34d76df4a2065af1a5ba.jpg','https://telegra.ph/file/05c1b22ee83bcd7723b4d.jpg','https://telegra.ph/file/1d886f66a44871205335f.jpg','https://telegra.ph/file/54d19a9094dc509370bf9.jpg','https://telegra.ph/file/e649475bcde78a9977ee5.jpg','https://telegra.ph/file/32ba20b6139b129c559c8.jpg','https://telegra.ph/file/948434cda49e4af5d9f11.jpg','https://telegra.ph/file/6f353c68533283fe79871.jpg','https://telegra.ph/file/de268ca9b01101acf2b8a.jpg','https://telegra.ph/file/fc6c5b2ae9a20c4256e7f.jpg', 'https://telegra.ph/file/efb70bb0988640f841742.jpg', 'https://telegra.ph/file/77d03fff530a2bcff3bf7.jpg','https://telegra.ph/file/6e4623464a659dd8d902b.jpg','https://telegra.ph/file/685aa39f0cb0f2c4fd85b.jpg','https://telegra.ph/file/10454b9ad693e1eefea58.jpg','https://telegra.ph/file/7de8ce5c290c3d8be0856.jpg','https://telegra.ph/file/8d7c4eadb7a4722747074.jpg','https://telegra.ph/file/ccc5f8eaac0f30919ef6c.jpg','https://telegra.ph/file/95f4b43439d7888f15ea5.jpg','https://telegra.ph/file/9c2a750db555bd2fad1f3.jpg', 'https://telegra.ph/file/efc5f7e637744fd6bfec2.jpg','https://telegra.ph/file/3a5231aade245665633bd.jpg','https://telegra.ph/file/2ecfc76feb26ec1eab99b.jpg','https://telegra.ph/file/dabb70983b4e833d98344.jpg','https://telegra.ph/file/75193a893e38fc580afe6.jpg','https://telegra.ph/file/217aa0f4ec76273808aa4.jpg','https://telegra.ph/file/8a35d3446b97ae22c7b23.jpg','https://telegra.ph/file/092df720701575a7ceaaa.jpg','https://telegra.ph/file/a65184a676cd648de34c3.jpg','https://telegra.ph/file/180e28807e78419d45537.jpg','https://telegra.ph/file/140eff27be983e0cd6781.jpg','https://telegra.ph/file/1581b791e16d0029e16b5.jpg','https://telegra.ph/file/6a4b36372b4f265bae3bc.jpg','https://telegra.ph/file/093caff422f194f00bc6c.jpg','https://telegra.ph/file/2294b7ab49eca8a8046b2.jpg','https://telegra.ph/file/869224d1c417e8b5c8ff1.jpg','https://telegra.ph/file/a78443f0ee887f46808d7.jpg','https://telegra.ph/file/1889878933264d16c58bf.jpg','https://telegra.ph/file/735aeb47d9c4aa87aaaf3.jpg','https://telegra.ph/file/fcf861516db09dda164e0.jpg','https://telegra.ph/file/355d96d7e06d109435f67.jpg']
 global.hwaifu = ['https://telegra.ph/file/2cd3626c740ba091c98ff.jpg','https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg', 'https://telegra.ph/file/590f94b0907c9cafb7494.jpg', 'https://telegra.ph/file/2cd3626c740ba091c98ff.jpg', 'https://telegra.ph/file/f8e9f27ae13fc71e1fde7.jpg', 'https://telegra.ph/file/590f94b0907c9cafb7494.jpg', 'https://telegra.ph/file/ddda3405b847c58198684.jpg']
 global.hyuri = ['https://telegra.ph/file/5d908f4a17515a15c6202.jpg']
 global.hLokun = ['https://telegra.ph/file/8902f4fc550727a62e99f.jpg','https://telegra.ph/file/6a6a40e924c16a8f0de03.jpg', 'https://telegra.ph/file/b035d3038a0b124f1d846.jpg', 'https://telegra.ph/file/9d475f7852bf6f6193c80.jpg']
@@ -206,10 +206,10 @@ global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.shee
 global.dpdf = 'application/pdf'
 global.drtf = 'text/rtf'
 
-global.thumbdoc = 'https://telegra.ph/file/6e45318d7c76f57e4a8bd.jpg'
+global.thumbdoc = '___'
 
 /*=========== FAKE SIZE ===========*/
-global.fsizedoc = '9999999' // default 10TB
+global.fsizedoc = '99999' // default 10TB
 global.fpagedoc = '404'
 
 /*=========== HIASAN ===========*/
